929117293fc60e8718b18714bc52610573b0a6f9
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2023-09-16 19:45:32 +0000

    Add summernote editor and admin panel interface

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from .models import Recipe, Country
+from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Recipe)
-class RecipeAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title',)
+class RecipeAdmin(SummernoteModelAdmin):
+    """
+    Specifies how the admin panel is displayed
+    Allows the use of summernote editor for selected field
+    """
+    list_display = ('id', 'title', 'post_approved', 'date_created')
+    search_fields = ['title', 'cook_time', 'country'] 
+    list_filter = ('date_created', 'post_approved', 'country')
+    summernote_fields = ('ingredients', 'instructions')
 
 
 @admin.register(Country)
