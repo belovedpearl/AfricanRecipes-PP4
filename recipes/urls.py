6977948ea4c27554003281477ebbd3f0cd095569
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2023-09-19 19:39:17 +0000

    Add template and url path for delete recipe post

--- recipes/urls.py
+++ recipes/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('details/<int:pk>', views.RecipeDetails.as_view(), name='recipe-detail'),
     path('addrecipe/', views.AddRecipe.as_view(), name='add_recipe'),
     path('updaterecipe/edit/<int:pk>', views.UpdateRecipe.as_view(), name='updaterecipe'),
+    path('details/<int:pk>/delete', views.DeleteRecipe.as_view(), name='deleterecipe'),
     
 ]
