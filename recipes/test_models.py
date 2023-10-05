2e319e5a2d0f4a27986e0e19e82fbb2879391f69
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2023-10-05 03:19:25 +0000

    Add test to check for primary key addition and lax-length of recipe title

--- recipes/test_models.py
+++ recipes/test_models.py
@@ -10,7 +10,6 @@ class TestModel(TestCase):
         self.user = User.objects.create_user(username='test_user', password='test_password')
         self.country = Country.objects.create(name='test_country')
   
-
     def test_recipe_creation(self):
         # Test for complete recipe creation
         recipe = Recipe.objects.create(
@@ -31,7 +30,30 @@ class TestModel(TestCase):
         self.assertEqual(recipe.cook_time, 120)
         self.assertEqual(recipe.country, self.country)
 
-    # def test_recipe_title_max_length(self):
-    #     recipe = Recipe.objects.create(pk=1)
-    #     max_length = recipe.meta.get_field('title')
-    #     self.assertEqual(max_length, 150)
+    def test_recipe_primary_key(self):
+        Recipe.objects.create( 
+            title='Test Recipe', 
+            author=self.user, 
+            ingredients='Test ingredients', 
+            instructions='Test instructions', 
+            recipe_image='test_image.png', 
+            cook_time=120, 
+            country=self.country,)
+        recipe = Recipe.objects.get(title='Test Recipe')
+        self.assertIsNotNone(recipe.pk)
+        self.assertIsInstance(recipe.pk, int)
+        expected_pk = 1
+        self.assertEqual(recipe.pk, expected_pk)
+
+    def test_recipe_title_max_length(self):
+        Recipe.objects.create( 
+            title='Test Recipe', 
+            author=self.user, 
+            ingredients='Test ingredients', 
+            instructions='Test instructions', 
+            recipe_image='test_image.png', 
+            cook_time=120, 
+            country=self.country,)
+        recipe = Recipe.objects.get(pk=1)
+        max_length = recipe._meta.get_field('title').max_length
+        self.assertEqual(max_length, 150)
