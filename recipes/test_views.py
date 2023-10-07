9600b9673c4234eadd0e3c2b0ca30f2bc1fe95d4
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2023-10-07 05:46:59 +0000

    Check correct rendering of home page

--- recipes/test_views.py
+++ recipes/test_views.py
@@ -1,6 +1,28 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import Recipe, Country
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-class textword(TestCase):
 
-    def test_this_thing_works(self):
-        self.assertEqual(1, 1)
+class TestViews(TestCase):
+    def test_correct_landing_page_used(self):
+        """
+        Test home page renders correctly
+        Test that right template is used
+        """
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+
+   
+
+
+
+
+
+
+
+
+
+
+
