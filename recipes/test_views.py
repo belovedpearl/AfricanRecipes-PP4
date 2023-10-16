babb28f455623b7b490806244a31619474f4f52d
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2023-10-16 09:18:23 +0000

    Format test_view file

--- recipes/test_views.py
+++ recipes/test_views.py
@@ -5,21 +5,22 @@ from django.contrib.auth.models import User
 
 
 class TestViews(TestCase):
-    
     def setUp(self):
         # Create a setup test user and country
-        self.user = User.objects.create_user(username='Test User', password='Test Password')
-        self.country = Country.objects.create(name='Test Country')
+        self.user = User.objects.create_user(
+            username="Test User", password="Test Password"
+        )
+        self.country = Country.objects.create(name="Test Country")
 
         # Setup a test recipe
         self.recipe = Recipe.objects.create(
-           title='Test Recipe',
-           author=self.user,
-           ingredients='Test ingredients',
-           instructions='Test instructions',
-           recipe_image='test_image.png',
-           cook_time=120,
-           country=self.country,
+            title="Test Recipe",
+            author=self.user,
+            ingredients="Test ingredients",
+            instructions="Test instructions",
+            recipe_image="test_image.png",
+            cook_time=120,
+            country=self.country,
         )
 
     def test_correct_landing_page_used(self):
@@ -27,9 +28,9 @@ class TestViews(TestCase):
         Test home page renders correctly
         Test that right template is used
         """
-        response = self.client.get('/')
+        response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'index.html')
+        self.assertTemplateUsed(response, "index.html")
 
     def test_get_view_details_page(self):
         """
@@ -39,66 +40,59 @@ class TestViews(TestCase):
         client = Client()
 
         # Sigin the user
-        client.login(username='Test User', password='Test Password')
-        url = reverse('recipe-detail', args=[self.recipe.pk])
+        client.login(username="Test User", password="Test Password")
+        url = reverse("recipe-detail", args=[self.recipe.pk])
 
         response = client.get(url)
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'details.html')
+        self.assertTemplateUsed(response, "details.html")
 
         # Check the content of the response
-        self.assertEqual(response.context['recipe'], self.recipe)
-        self.assertFalse(response.context['liked'])
-        self.assertFalse(response.context['disliked'])
+        self.assertEqual(response.context["recipe"], self.recipe)
+        self.assertFalse(response.context["liked"])
+        self.assertFalse(response.context["disliked"])
 
     def test_get_add_recipe_page(self):
         """
         Test if add_recipe page renders correctly
         """
-        response = self.client.get('/addrecipe/')
+        response = self.client.get("/addrecipe/")
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'addrecipe.html')
+        self.assertTemplateUsed(response, "addrecipe.html")
 
     def test_get_update_recipe_page(self):
         """
         Test if update recipe page renders correctly
         """
-        url = reverse('updaterecipe', kwargs={'pk': self.recipe.pk})
+        url = reverse("updaterecipe", kwargs={"pk": self.recipe.pk})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'updaterecipe.html')
+        self.assertTemplateUsed(response, "updaterecipe.html")
 
     def test_get_delete_recipe_page(self):
         """
         Test if delete recipe page renders correctly
         """
-        url = reverse('deleterecipe', kwargs={'pk': self.recipe.pk})
+        url = reverse("deleterecipe", kwargs={"pk": self.recipe.pk})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'deleterecipe.html')
+        self.assertTemplateUsed(response, "deleterecipe.html")
 
     def test_get_country_page(self):
         """
         Test country page renders succesfully
         """
-        url = reverse('countries', kwargs={'choice': 'Test Country'})
+        url = reverse("countries", kwargs={"choice": "Test Country"})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'countries.html')
-    
+        self.assertTemplateUsed(response, "countries.html")
+
     def test_get_edit_user_page(self):
         """
         Test if edit user page renders correctly
         """
-        self.client.login(username='Test User', password='Test Password')
-        url = reverse('edit_profile')
+        self.client.login(username="Test User", password="Test Password")
+        url = reverse("edit_profile")
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'edit_profile.html')
-
-
-
-
-
-
-
+        self.assertTemplateUsed(response, "edit_profile.html")
