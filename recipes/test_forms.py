2ba184fdee6f84dab3a26a533b10aeea037cd6d4
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2023-10-16 09:16:37 +0000

    Format tes_forms file

--- recipes/test_forms.py
+++ recipes/test_forms.py
@@ -13,14 +13,22 @@ class TestItemForm(TestCase):
         self.assertFalse(form.is_valid())
         self.assertIn("title", form.errors.keys())
         self.assertEqual(form.errors["title"][0], "This field is required.")
-    
+
     def test_fields_are_explicit_in_form_metaclass(self):
         """
         Test form fields
-        Checks if represented form field is the same as the actual fields 
+        Checks if represented form field is the same as the actual fields
         """
         form = RecipeForm()
-        formfields = ["title", "author", "country","ingredients", "instructions", "cook_time", "recipe_image"]
+        formfields = [
+            "title",
+            "author",
+            "country",
+            "ingredients",
+            "instructions",
+            "cook_time",
+            "recipe_image",
+        ]
         actualfield = list(form.fields.keys())
         self.assertEqual(formfields, actualfield)
 
@@ -31,14 +39,16 @@ class TestEditProfileForm(TestCase):
         Test edit profile form
         """
         # Create a user instance for testing
-        user = User.objects.create_user(username='testuser', password='testpassword')
+        user = User.objects.create_user(
+            username="testuser", password="testpassword"
+        )
 
         # Prepare valid form data
         form_data = {
-            'email': 'test@yes.com',
-            'first_name': 'Ade',
-            'last_name': 'Ola',
-            'username': 'newuser',
+            "email": "test@yes.com",
+            "first_name": "Ade",
+            "last_name": "Ola",
+            "username": "newuser",
         }
         form = EditProfileForm(data=form_data, instance=user)
         self.assertTrue(form.is_valid())
@@ -46,17 +56,19 @@ class TestEditProfileForm(TestCase):
     def test_edit_profile_form_invalid_data(self):
         """
         Test form
-        Check form validity with empty username fielf 
+        Check form validity with empty username fielf
         """
         # Create a user instance for testing
-        user = User.objects.create_user(username='testuser', password='testpassword')
+        user = User.objects.create_user(
+            username="testuser", password="testpassword"
+        )
 
         # Check with empty username
         form_data = {
-            'email': 'test@yes.com',
-            'first_name': 'Ade',
-            'last_name': 'Ola',
-            'username': '',
+            "email": "test@yes.com",
+            "first_name": "Ade",
+            "last_name": "Ola",
+            "username": "",
         }
         form = EditProfileForm(data=form_data, instance=user)
         self.assertFalse(form.is_valid())
@@ -66,8 +78,8 @@ class EditUserViewTest(TestCase):
     def setUp(self):
         # A representative test user
         self.user = User.objects.create_user(
-            username='testuser',
-            password='testpassword',
+            username="testuser",
+            password="testpassword",
         )
 
     def test_successful_form_submission(self):
@@ -75,17 +87,15 @@ class EditUserViewTest(TestCase):
         Test for successful form submission
         Test page redirect after form submission
         """
-        self.client.login(username='testuser', password='testpassword')
+        self.client.login(username="testuser", password="testpassword")
 
         # Redine user's details to submit
         form_data = {
-            'username': 'test_user_name',
-            'first_name': 'NewFirst Name',
-            'last_name': 'NewLast Name',
-            'email': 'newemail@yes.com',
+            "username": "test_user_name",
+            "first_name": "NewFirst Name",
+            "last_name": "NewLast Name",
+            "email": "newemail@yes.com",
         }
-        response = self.client.post(reverse('edit_profile'), form_data)
+        response = self.client.post(reverse("edit_profile"), form_data)
         self.assertEqual(response.status_code, 302)
-        self.assertRedirects(response, reverse('home'))
-
-
+        self.assertRedirects(response, reverse("home"))
