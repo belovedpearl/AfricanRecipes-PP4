113da5e5f85d9abb2353e5e35bc3bb9fa447ceb4
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2023-10-28 12:45:07 +0000

    Remove unwanted code and properly write post method on forms

--- recipes/forms.py
+++ recipes/forms.py
@@ -12,7 +12,6 @@ class RecipeForm(forms.ModelForm):
         model = Recipe
         fields = (
             "title",
-            # "author",
             "country",
             "ingredients",
             "instructions",
@@ -26,7 +25,6 @@ class RecipeForm(forms.ModelForm):
                     "placeholder": "Recipe Local Name",
                 }
             ),
-            # "author": forms.Select(attrs={"class": "form-control"}),
             "country": forms.Select(attrs={"class": "form-control"}),
             "ingredients": SummernoteWidget(attrs={"class": "form-control"}),
             "instructions": SummernoteWidget(attrs={"class": "form-control"}),
@@ -38,28 +36,6 @@ class RecipeForm(forms.ModelForm):
                 }
             ),
         }
-
-    # def __init__(self, *args, **kwargs):
-    #     """
-    #     Form helper to generate form fields
-    #     """
-    #     super().__init__(*args, **kwargs)
-    #     self.helper = FormHelper()
-    #     self.helper.layout = Layout(
-    #         Field("title"),
-    #         # Field("author"),
-    #         Field("country"),
-    #         Field("ingredients", css_class="editor"),
-    #         Field("instructions", css_class="editor"),
-    #         Field("recipe_image"),
-    #         Field("cook_time"),
-    #         Submit(
-    #             "submit",
-    #             "Submit",
-    #             css_class="btn btn-secondary btn-lg",
-    #             style="margin-bottom: 10px;",
-    #         ),
-    #     )
 
 
 class EditProfileForm(forms.ModelForm):
